c7929ee10ea46549469d93a34756970d877e00a6
BGorson <BGorson32@gmail.com>
2019-02-10 17:43:25 -0600

    major updates

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,27 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function responsive() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunctionSticky()};
+
+// Get the navbar
+var navbar = document.getElementById("stickyBar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunctionSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
